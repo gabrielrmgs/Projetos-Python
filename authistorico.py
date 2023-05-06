e3c7f7c82150e1428b62af208f5eb7702596aed0
rmg <gabrielrdes@outlook.com>
2023-05-06 15:02:54 -0300

    baixando historico UESPI

--- authistorico.py
+++ authistorico.py
@@ -3,12 +3,18 @@ from selenium import webdriver
 #from webdriver_manager.chrome import ChromeDriverManager
 import time
 import tabula
+import os
+from selenium.webdriver.firefox.options import Options
 
+############### dando erroooOO""""$#%¨&*()@@@@@@@#$###########
 matricula = input("Digite a matricula: ")
 
 senha = input("Digite a senha: ")
 
+#options = Options()
+#options.add_argument('-headless')
 nav = webdriver.Firefox()
+
 
 '''
  #testando modulo de gerenciamento#
@@ -32,7 +38,7 @@ nav.find_element(By.XPATH, '/html/body/div[3]/div/div/div[3]/a').click()
 
 #nav.find_element(By.XPATH,'/html/body/div[2]/nav/div/div[2]/ul[1]/li[3]/a').click()
 
-elemento = nav.find_element(By.XPATH,'/html/body/div[2]/nav/div/div[2]/ul[1]/li[3]/a')
+elemento = nav.find_element(By.XPATH,'/html/body/div[2]/nav/div/div[2]/ul[1]/li[4]/a')
 nav.execute_script("arguments[0].click();", elemento)
 
 '''
@@ -43,9 +49,14 @@ rolada(nav)\
     .rolar(curric)\
     .perform()
 '''
-time.sleep(3)
-nav.find_element(By.XPATH,'/html/body/div[5]/div[1]/div/div[1]/div[2]/div[2]/form/input[3]').click()
+#time.sleep(3)
+elemento1 = nav.find_element(By.XPATH,'/html/body/div[2]/nav/div/div[2]/ul[1]/li[4]/ul/li[1]/a')
+nav.execute_script("arguments[0].click();", elemento1)
 
+time.sleep(3)
+nav.find_element(By.XPATH,'/html/body/div[5]/div/div/p[2]/a').click()
+
+#nav.quit()
 #baixar arquivo dando erro
 #time.sleep(3)
 #nav.find_element(By.XPATH, '//*[@id="download"]').click()
@@ -59,14 +70,23 @@ print(f"Curriculo matricula: {link}")
 '''
 
 '''
+testando explorar arquivos na maquina
+arquivos = os.listdir("/home/rmg/Downloads")
+
+os.rename
+
+'''
+#path_dir = "/home/rmg/Downloads/historico.pdf"
+#os.startfile(path_dir)
+
 #implementação de leitura e transcrição do pdf historico em arquivo.txt usando tabula-py
 
-lista_tabel = tabula.read_pdf("historico.pdf", pages="all")
+#lista_tabel = tabula.read_pdf("historico.pdf", pages="all")
 
 #for i in lista_tabel:
     #print(i)
 
-
+'''
 g = open("notas.txt", "w")
 g.write(str(lista_tabel[0]))
 g.close()
